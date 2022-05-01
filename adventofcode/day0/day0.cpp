a280cbdc41b09d29065aa3211a8773eb1bdaed23
klishevich <busation@gmail.com>
2022-05-01 21:13:14 +0300

    day 0

---
+++ adventofcode/day0/day0.cpp
@@ -0,0 +1,50 @@
+// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <queue>
+#include <sstream>
+
+using namespace std;
+
+string input() {
+    return R"(2199943210
+3987894921
+9856789892
+8767896789
+9899965678
+)";
+}
+
+int main() {
+    string line;
+    vector<string> area;
+    for (stringstream file(input()); getline(file, line);)
+        area.push_back(line);
+
+    priority_queue<int> top3;
+    size_t m = size(area), n = size(area[0]), res = 1;
+    for (int i = 0; i < m; ++i)
+        for (int j = 0; j < n; ++j) {
+            // iterative DFS
+            int basin = 0;
+            vector<pair<int, int>> stack{ {i, j} };
+            while (size(stack)) {
+                auto [r, c] = stack.back();
+                stack.pop_back();
+                if (r < 0 || c < 0 || r == m || c == n) continue;
+                if (exchange(area[r][c], '9') == '9') continue;
+                ++basin;
+                stack.insert(end(stack), { {r + 1,c}, {r - 1,c}, {r,c + 1}, {r,c - 1} });
+            }
+
+            // maintain 3 largest basins
+            if (basin) top3.push(-basin);
+            if (top3.size() > 3) top3.pop();
+        }
+
+    // product of 3 largest basins
+    for (; top3.size(); top3.pop())
+        res *= -top3.top();
+    cout << "Part 2: " << res << endl;
+}
