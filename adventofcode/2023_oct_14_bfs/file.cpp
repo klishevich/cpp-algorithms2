9f08e2eba9807276ed6a23eed6d79269bec78db7
Michael Klishevich <busation@gmail.com>
2023-10-22 23:22:31 +0300

    pacman BFS algorithm improvement

--- adventofcode/2023_oct_14_bfs/file.cpp
+++ adventofcode/2023_oct_14_bfs/file.cpp
@@ -7,7 +7,13 @@
 
 using namespace std;
 
-typedef pair<size_t,size_t> row_col_type;
+typedef pair<int,int> row_col_pair;
+
+typedef struct {
+	int row;
+	int col;
+	int step;
+} row_col_step_struct;
 
 template <typename T>
 void print_vec(vector<T> &vec)
@@ -21,61 +27,59 @@ void print_vec(vector<T> &vec)
 }
 
 int main() {
-	size_t ROWS = 19;
-	size_t COLS = 21;
+	int ROWS = 19;
+	int COLS = 21;
 	vector<vector<bool>> maze_matrix;
 	string str;
-	for (size_t i=0; i<ROWS; i++) {
+	for (int i=0; i<ROWS; i++) {
 		vector<bool> maze_line;
 		cin >> str;
-		for (size_t j=0; j<COLS; j++) {
-			maze_line.push_back(str[j] == '0' ? false : true);
+		for (int j=0; j<COLS; j++) {
+			maze_line.push_back(str[(size_t)j] == '0' ? false : true);
 		}
 		maze_matrix.push_back(maze_line);
 	}
 
 	// print_vec<vector<bool>>(maze_matrix);
 
-	map<row_col_type,row_col_type> visited_prev_map;
-	deque<row_col_type> bfs_queue;
+	map<row_col_pair,row_col_pair> visited_prev_map;
+	deque<row_col_step_struct> bfs_queue;
 
-	row_col_type start_position = {1, 15};
-	row_col_type end_position1 = {9, 0};
-	row_col_type end_position2 = {9, 20};
-	bfs_queue.push_back(start_position);
+	row_col_step_struct start_pos = {1, 15, 0};
+	row_col_step_struct end_pos1 = {9, 0, 0};
+	row_col_step_struct end_pos2 = {9, 20, 0};
+	bfs_queue.push_back(start_pos);
+	size_t queue_index = 0;
+
+	vector<pair<int,int>> adj_cells = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
 	do {
-		row_col_type cur = bfs_queue.front();
-		if (cur == end_position1 || cur == end_position2) break;
-		bfs_queue.pop_front();
-		size_t row = cur.first;
-		size_t col = cur.second;
-		if (row>0 && maze_matrix[row-1][col]==0 && !visited_prev_map.count({row-1, col})) {
-			visited_prev_map.insert({{row-1, col}, cur});
-			bfs_queue.push_back({row-1, col});
-		}
-		if (row<ROWS-1 && maze_matrix[row+1][col]==0 && !visited_prev_map.count({row+1,col})) {
-			visited_prev_map.insert({{row+1, col}, cur});
-			bfs_queue.push_back({row+1, col});
-		}
-		if (col>0 && maze_matrix[row][col-1]==0 && !visited_prev_map.count({row,col-1})) {
-			visited_prev_map.insert({{row, col-1}, cur});
-			bfs_queue.push_back({row, col-1});
-		}
-		if (col<COLS-1 && maze_matrix[row][col+1]==0 && !visited_prev_map.count({row,col+1})) {
-			visited_prev_map.insert({{row, col+1}, cur});
-			bfs_queue.push_back({row, col+1});
-		}
-	}
-	while (bfs_queue.size() > 0);
+		row_col_step_struct s = bfs_queue[queue_index];
+		if ((s.row == end_pos1.row && s.col == end_pos1.col)
+			|| (s.row == end_pos2.row && s.col == end_pos2.col)) break;
 
-	row_col_type r = bfs_queue.front();
-	cout << "(" << r.first << "," << r.second << ")" << endl;
+		for (auto p : adj_cells) {
+			int row = s.row + p.first;
+			int col = s.col + p.second;
+			if (row>=0 && row<ROWS && col>=0 && col<COLS
+				&& maze_matrix[(size_t)row][(size_t)col]==0 && !visited_prev_map.count({row, col})) {
+				visited_prev_map.insert({{row,col}, {s.row, s.col}});
+				row_col_step_struct new_element = { row, col, s.step + 1};
+				bfs_queue.push_back(new_element);
+			}
+		}
+		queue_index++;
+	}
+	while (queue_index < bfs_queue.size());
+
+	row_col_step_struct e = bfs_queue[queue_index];
+	cout << "(" << e.row << "," << e.col << ")" << endl;
+	row_col_pair r = { e.row, e.col };
 	do {
 		r = visited_prev_map.find(r)->second;
 		cout << "(" << r.first << "," << r.second << ")" << endl;
 	}
-	while (r != start_position);
+	while (r.first != start_pos.row || r.second != start_pos.col );
 
 	cout << "End" << endl;
 }
