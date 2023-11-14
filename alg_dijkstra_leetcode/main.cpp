748d537c5d806a9964c4c9d6428f8c0b9c4f475b
Michael Klishevich <busation@gmail.com>
2023-11-14 11:39:04 +0300

    Dijkstra algorithm for leetcode finished

--- alg_dijkstra_leetcode/main.cpp
+++ alg_dijkstra_leetcode/main.cpp
@@ -5,53 +5,13 @@
 #include <set>
 #include <queue>
 #include <map>
+#include <limits.h>
 
 using namespace std;
 
 int SIZE = 9;
 
 string input();
-
-class Graph {
-private:
-    map<int, map<int, int>> g;
-    set<int> visited;
-    map<int, int> dist;
-public:
-    Graph(int n, vector<vector<int>>& edges) {
-        for (int i=0; i<edges.size(); ++i) {
-            this->addEdge(edges[i]);
-        } 
-    }
-    
-    void addEdge(vector<int> edge) {
-        int from = edge[0];
-        int to = edge[1];
-        int cost = edge[2];
-        if (g.find(from) == g.end()) {
-            g.insert({from, map<int, int>() });
-        }
-        g.find(from)->second.insert({to, cost});
-    }
-    
-    int shortestPath(int node1, int node2) {
-        // TODO
-    }
-
-    void print() {
-        for (const auto &[k, v] : g) {
-            cout << k << "=>" << endl;
-            for (const auto &[k1, v1] : v)
-                cout << " " << k1 << ":" << v1 << endl;
-        }
-        // for (auto v : g) {
-        //     for (auto c : v)
-        //         cout << c << " ";
-        //     cout << endl;
-        // }
-        // cout << endl;
-    }
-};
 
 class Vertex {
 public:
@@ -66,11 +26,78 @@ public:
 
     bool operator<(const Vertex& v) const
     {
-        // this will return true when second vertex
-        // has lower priority. Suppose we have v1.priority=6
-        // and v2.priority=5 then the object which
         // has min priority will be at the top
         return this->priority > v.priority;
+    }
+};
+
+class Graph {
+private:
+    map<int, map<int, int>> g;
+    int n;
+public:
+    Graph(int n, vector<vector<int>>& edges) {
+        this->n = n;
+        for (int i=0; i<edges.size(); ++i) {
+            this->add_edge(edges[i]);
+        } 
+    }
+    
+    void add_edge(vector<int> edge) {
+        int from = edge[0];
+        int to = edge[1];
+        int cost = edge[2];
+        if (g.find(from) == g.end()) {
+            g.insert({from, map<int, int>() });
+        }
+        g.find(from)->second.insert({to, cost});
+    }
+    
+    int shortest_path(int node1, int node2) {
+        set<int> visited;
+        priority_queue<Vertex> vertices_queue;
+        map<int, int> distances;
+
+        vertices_queue.push(Vertex(node1, 0));
+        distances.insert({node1, 0});
+
+        do {
+            Vertex u = vertices_queue.top();
+            vertices_queue.pop();
+
+            if (visited.count(u.id)) continue;
+
+            visited.insert(u.id);
+            int u_dist = distances.at(u.id);
+
+            if (u.id == node2) return u_dist;
+
+            if (g.find(u.id) == g.end()) continue;
+            map<int, int> routes_to = g.find(u.id)->second;
+             
+            for (auto const& [v_id, u_v_dist] : routes_to) {
+                if (visited.count(v_id)) continue;
+
+                int v_cur_dist = distances.find(v_id) == distances.end() ? INT_MAX : distances.find(v_id) -> second;
+                int v_new_dist = u_dist + u_v_dist;
+                if (v_new_dist < v_cur_dist) {
+                    distances[v_id] = v_new_dist;
+                    vertices_queue.push(Vertex(v_id, v_new_dist));
+                }
+            }
+
+        } while (vertices_queue.size() > 0);
+
+        return -1;
+    }
+
+    void print() {
+        cout << "n " << n << endl;
+        for (const auto &[k, v] : g) {
+            cout << k << "=>" << endl;
+            for (const auto &[k1, v1] : v)
+                cout << " " << k1 << ":" << v1 << endl;
+        }
     }
 };
 
@@ -107,35 +134,20 @@ vector<vector<int>> read_edges()
     return graph;
 }
 
-void print_visited(set<int> visited) {
-    cout << "visited: ";
-    for (int i=0; i<SIZE; i++) {
-        string v = visited.count(i)? "+": to_string(i);
-        cout << v << " ";
-    }
-    cout << endl;
-}
-
-void print_queue(priority_queue<Vertex> q) {
-    cout << "queue: ";
-    do {
-        Vertex a = q.top();
-        cout << a.id << "(" << a.priority << ")" << " ";
-        q.pop();
-    } while (q.size() > 0);
-    cout << endl;
-}
-
 int main()
 {
     // input
     vector<vector<int>> edges = read_edges();
     print_vec<vector<int>>(edges);
 
-    Graph* obj = new Graph(4, edges);
-    obj->print();
+    Graph* graph = new Graph(4, edges);
+    graph->print();
 
-    int param_2 = obj->shortestPath(3,2);
+    cout << "shortest 3 -> 2 (6): " << graph->shortest_path(3,2) << endl;
+    cout << "shortest 0 -> 3 (-1): " << graph->shortest_path(0,3) << endl;
+    graph->add_edge(vector<int>({1, 3, 4}));
+    // graph->print();
+    cout << "shortest 0 -> 3 (6): " << graph->shortest_path(0,3) << endl;
     return 0;
 }
 
